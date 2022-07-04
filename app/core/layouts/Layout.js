ea08891d9fdfae0c1520f5512b3a9217909d3005
Dejan Jacimovic <dejan@stuntcoders.com>
2022-07-04 17:49:42 +0200

    Add meme as favicon because other problems are killing me

--- app/core/layouts/Layout.js
+++ app/core/layouts/Layout.js
@@ -5,7 +5,10 @@ const Layout = ({ title, children }) => {
     <>
       <Head>
         <title>{title || "reo_web"}</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="icon"
+          href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🏡</text></svg>"
+        />
       </Head>
 
       {children}
